6f224e1a9cdcc9ecabf59d9dcf802314f94d914b
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2015-07-07 14:10:35 +0200

    implementing check if repository

--- src/kevoree-main.c
+++ src/kevoree-main.c
@@ -119,29 +119,17 @@ void actionstore(char *path, Type type, void *value)
 
 static const char *DEFAULTMODEL = "{\"eClass\":\"org.kevoree.ContainerRoot\",\"generated_KMF_ID\":\"BXX5q3eV\",\"nodes\":[{\"eClass\":\"org.kevoree.ContainerNode\",\"name\":\"n1759\",\"metaData\":\"\",\"started\":\"true\",\"components\":[],\"hosts\":[],\"host\":[],\"groups\":[\"groups[group0]\"],\"networkInformation\":[{\"eClass\":\"org.kevoree.NetworkInfo\",\"name\":\"ip\",\"values\":[{\"eClass\":\"org.kevoree.NetworkProperty\",\"name\":\"local\",\"value\":\"fe80::1759\"}]}],\"typeDefinition\":[\"typeDefinitions[ContikiNode/0.0.1]\"],\"dictionary\":[],\"fragmentDictionary\":[]}],\"typeDefinitions\":[{\"eClass\":\"org.kevoree.NodeType\",\"name\":\"ContikiNode\",\"version\":\"0.0.1\",\"factoryBean\":\"\",\"bean\":\"\",\"abstract\":\"0\",\"deployUnit\":[\"deployUnits[org.kevoree.library.c//kevoree-contiki-node/0.0.1]\"],\"dictionaryType\":[],\"superTypes\":[]},{\"eClass\":\"org.kevoree.GroupType\",\"name\":\"UDPGroup\",\"version\":\"0.0.1\",\"factoryBean\":\"\",\"bean\":\"\",\"abstract\":\"0\",\"deployUnit\":[\"deployUnits[//kevoree-group-udp/0.0.1]\"],\"dictionaryType\":[{\"eClass\":\"org.kevoree.DictionaryType\",\"generated_KMF_ID\":\"3dddTFpd\",\"attributes\":[{\"eClass\":\"org.kevoree.DictionaryAttribute\",\"name\":\"port\",\"optional\":\"false\",\"state\":\"true\",\"datatype\":\"int\",\"fragmentDependant\":\"false\",\"defaultValue\":\"1234\"}]}],\"superTypes\":[]}],\"repositories\":[],\"dataTypes\":[],\"libraries\":[{\"eClass\":\"org.kevoree.TypeLibrary\",\"name\":\"ContikiLib\",\"subTypes\":[\"typeDefinitions[ContikiNode/0.0.1]\",\"typeDefinitions[UDPGroup/0.0.1]\"]},{\"eClass\":\"org.kevoree.TypeLibrary\",\"name\":\"Default\",\"subTypes\":[]}],\"hubs\":[],\"mBindings\":[],\"deployUnits\":[{\"eClass\":\"org.kevoree.DeployUnit\",\"name\":\"kevoree-group-udp\",\"groupName\":\"\",\"version\":\"0.0.1\",\"url\":\"\",\"hashcode\":\"\",\"type\":\"ce\"},{\"eClass\":\"org.kevoree.DeployUnit\",\"name\":\"kevoree-contiki-node\",\"groupName\":\"org.kevoree.library.c\",\"version\":\"0.0.1\",\"url\":\"\",\"hashcode\":\"\",\"type\":\"ce\"}],\"nodeNetworks\":[],\"groups\":[{\"eClass\":\"org.kevoree.Group\",\"name\":\"group0\",\"metaData\":\"\",\"started\":\"true\",\"subNodes\":[\"nodes[n1759]\"],\"typeDefinition\":[\"typeDefinitions[UDPGroup/0.0.1]\"],\"dictionary\":[],\"fragmentDictionary\":[]}]}";
 
-int main(void)
+/* load model from a json file */
+ContainerRoot*
+open_model(const char* filename)
 {
-	
-	printf("INFO: Starting kevoree C implementation\n");
 	struct jsonparse_state jsonState;
-	TraceSequence *ts;
-
-	jsonparse_setup(&jsonState, DEFAULTMODEL, strlen(DEFAULTMODEL) + 1);
-	current_model = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
-
-	/*current_model->VT->visit(current_model, NULL, actionprintf, NULL, false);*/
-
-	printf("INFO: Starting Kevoree adaptations\n");
-
-	FILE *new_model_json = fopen("../models/20nodes1component.json", "r");
+	
+	FILE *new_model_json = fopen(filename, "r");
 	fseek(new_model_json, 0L, SEEK_END);
 	int modelLength = ftell(new_model_json);
 	fseek(new_model_json, 0L, SEEK_SET);
-
-	printf("INFO: Trying to load new_model with length %d\n", modelLength);
-
-	mtrace();
-
+	
 	char *jsonModel = malloc(modelLength + 1);
 	int ch;
 	bool firstChar = true;
@@ -158,34 +146,167 @@ int main(void)
 	}
 
 	fclose(new_model_json);
-
-	printf("INFO: new_model JSON loaded in RAM\n");
-
 	jsonparse_setup(&jsonState, jsonModel, modelLength + 1);
-
-	new_model = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
+	
+	ContainerRoot * model = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
 	free(jsonModel);
 	
-	//delete((KMFContainer*)new_model);
-	//exit(0);
+	return model;
+}
+
+/**************************************************************************************************
+ * all this is for implementing the checkIfRepository
+ **************************************************************************************************/
+int
+fillLocalAddress(any_t extra, any_t item)
+{
+	NetworkProperty* s = (NetworkProperty*) item;
+	char** address = (char**)extra;
+	*address = s->value; 
+	return MAP_OK;
+}
+
+int
+fillAddress(any_t extra, any_t item)
+{
+	NetworkInfo *nInfo = (NetworkInfo*)item;
+	hashmap_iterate(nInfo->values, fillLocalAddress, extra);
+	return MAP_OK;
+}
+
+struct NodeItem {
+	void* next;
+	char* nodeName;
+	char* nodeIp;
+	char* requiredDeployUnit;
+	int requiresDeployUnit;
+};
+
+int
+printComponents(any_t extra, any_t item)
+{
+	struct NodeItem* nodeItem = (struct NodeItem*) extra;
+	ComponentInstance* c = (ComponentInstance*)item;
+	TypeDefinition* type = (TypeDefinition*) c->typeDefinition;
+	DeployUnit* du = (DeployUnit*)type->deployUnits;
+	char* s = typeDefinition_VT.internalGetKey(type);
+	char* duName = deployUnit_VT.internalGetKey(du);
+	printf("\t%s : %s : %s : du => %s\n", c->name, type->name, s, duName);
+	if (strcmp(nodeItem->requiredDeployUnit, duName) == 0) {
+		nodeItem->requiresDeployUnit = true;
+	}
+	return MAP_OK;
+}
+
+struct SuperData {
+	list_t* l;
+	char* requiredDU;
+};
+
+int
+printNodes(any_t extraData, any_t item)
+{
+	struct NodeItem* nodeItem = (struct NodeItem*)malloc(sizeof(struct NodeItem));
+	ContainerNode *node = (ContainerNode*)item;
+	nodeItem->nodeName = node->name;
+	hashmap_iterate(node->networkInformation, fillAddress, &nodeItem->nodeIp);
+	
+	struct SuperData* data = (struct SuperData*)extraData;
+	
+	nodeItem->requiredDeployUnit = data->requiredDU;
+	nodeItem->requiresDeployUnit = 0;
+	
+	// print components and their type
+	printf("Node %s\n", node->name);
+	hashmap_iterate(node->components, printComponents, nodeItem);
+	
+	if (nodeItem->requiresDeployUnit) {
+		// add element to list
+		list_t l = *data->l;
+		list_add(l, nodeItem);
+	}
+	
+	
+	return MAP_OK;
+}
+
+int compare_ips(char* ip0, char* ip1)
+{
+	int l = strlen(ip0);
+	return (ip0[l-4] == ip1[l-4]) && (ip0[l-3] == ip1[l-3]) && (ip0[l-2] == ip1[l-2]) && (ip0[l-1] == ip1[l-1]);
+}
+
+void
+filter_nodes(ContainerRoot* model, int children_count, char* childrenIP[], const char* deployUnitType, list_t* r)
+{
+	struct SuperData superData = { .l = r, .requiredDU=deployUnitType };
+	// print address of each node
+	hashmap_iterate(model->nodes, printNodes , &superData);
+
+	// filter out elements if they are not in the list of children results
+	list_t nodes = *r;
+	LIST(tmp);
+	list_init(tmp);
+	for (int i = 0 ; i < children_count ; i++) {
+		struct NodeItem * found = NULL;
+		for (struct NodeItem* p =list_head(nodes); p != NULL ; p = list_item_next(p)) {
+			if (compare_ips(childrenIP[i], p->nodeIp)) {
+				found = p;
+			}
+		}
+		if (found) { 
+			list_remove(nodes, found);
+			list_push(tmp, found);
+		}
+	}
+	// now just empty the previous list
+	while (list_length(nodes) > 0) {
+		void* item = list_pop(nodes);
+		free(item);
+	}
+	
+	// now add elements to the empty list
+	while (list_length(tmp) > 0) {
+		void* item = list_pop(tmp);
+		list_push(nodes, item);
+	}
+}
+
+
+/********************************************************************************************/
+
+int main(void)
+{
+	
+	printf("INFO: Starting kevoree C implementation\n");
+	struct jsonparse_state jsonState;
+	TraceSequence *ts;
+
+	jsonparse_setup(&jsonState, DEFAULTMODEL, strlen(DEFAULTMODEL) + 1);
+	current_model = JSONKevDeserializer(&jsonState, jsonparse_next(&jsonState));
+
+	/*current_model->VT->visit(current_model, NULL, actionprintf, NULL, false);*/
+
+
+	mtrace();
+	
+	printf("INFO: Starting Kevoree adaptations\n");
+	new_model = open_model("models/20nodes1component.json");
 	
 	muntrace();
 
 	/*new_model->VT->visit(new_model, NULL, actionprintf, NULL, false);*/
 
-	if(new_model != NULL)
-	{
+	if(new_model != NULL) {
 		printf("INFO: new_model loaded successfully!\n");
 	}
-	else
-	{
+	else {
 		printf("ERROR: new_model cannot be loaded\n");
 	}
 
 	/*new_model->VT->visit(new_model, NULL, actionprintf, NULL, false);*/
 
-	if(new_model != NULL)
-	{
+	if(new_model != NULL) {
 		ts = ModelCompare(new_model, current_model, "n1759");
 		if (ts != NULL) {
 			/*char *tsString = ts->toString(ts);
@@ -195,8 +316,7 @@ int main(void)
 			printf("ERROR: Cannot create traceSequence\n");
 		}
 	}
-	else
-	{
+	else {
 		printf("ERROR: New model cannot be visited!\n");
 	}
 
@@ -222,8 +342,23 @@ int main(void)
 	ts->delete(ts);
 
 	delete((KMFContainer*)current_model);
-	//delete((KMFContainer*)new_model);
+	delete((KMFContainer*)new_model);
 	
+	printf("Info: Loading the Lille's model\n");
+	
+	ContainerRoot* model = open_model("models/10nodes1component-compactLille.json");
+	
+	
+	
+	LIST(nodes);
+	list_init(nodes);
+	char* children[] = {"fe80::3758", "fe80::3554"};
+	filter_nodes(model, 2, children, "kev_contiki//blink/0.0.1", &nodes);
+
+	// print the filter results
+	for (struct NodeItem* p =list_head(nodes); p != NULL ; p = list_item_next(p)) {
+		printf("Info: Node %s has address %s\n", p->nodeName, p->nodeIp);
+	}
 
 	return EXIT_SUCCESS;
 }
